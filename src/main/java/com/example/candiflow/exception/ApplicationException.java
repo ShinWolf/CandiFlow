a3b432fdecdb7209e073af34a352655e4db5ee0a
ShinWolf <77382557+ShinWolf@users.noreply.github.com>
2026-03-18 12:48:20 +0100

    Clean code

--- src/main/java/com/example/candiflow/exception/ApplicationException.java
+++ src/main/java/com/example/candiflow/exception/ApplicationException.java
@@ -1,8 +1,5 @@
 package com.example.candiflow.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class ApplicationException extends RuntimeException {
 
     public static class NotFoundException extends RuntimeException {
