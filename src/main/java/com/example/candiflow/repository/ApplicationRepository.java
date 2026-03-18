886c4e2a934861e2c5dc9223544395b74db0765f
ShinWolf <77382557+ShinWolf@users.noreply.github.com>
2026-03-18 13:16:09 +0100

    feat(application): add JpaSpecificationExecutor to repository

--- src/main/java/com/example/candiflow/repository/ApplicationRepository.java
+++ src/main/java/com/example/candiflow/repository/ApplicationRepository.java
@@ -2,11 +2,12 @@ package com.example.candiflow.repository;
 
 import com.example.candiflow.entity.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, Long>, JpaSpecificationExecutor<Application> {
     List<Application> findAllByUserId(Long userId);
     Optional<Application> findByIdAndUserId(Long id, Long userId);
 }
